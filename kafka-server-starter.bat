6d50cc06d67737bd796f4f4251e67619d4501f8b
Rishant Gupta <rgupta@operative.com>
2021-10-25 23:32:22 +0530

    SAGA Orchstration design pattern using Springboot with kafka event-store

---
+++ kafka-server-starter.bat
@@ -0,0 +1,2 @@
+cd D:\DEV_ENV\kafka
+start cmd /k .\bin\windows\kafka-server-start.bat .\config\server.properties
---
+++ zookeeper-starter.bat
@@ -0,0 +1,2 @@
+cd D:\DEV_ENV\kafka
+start cmd /k .\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
